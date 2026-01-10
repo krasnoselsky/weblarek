3046eedb4547b2ef7271cdc81de70bbcd43a871e
Artem Babayan <122205169+krasnoselsky@users.noreply.github.com>
2026-01-10 16:23:18 +0300

    Delete types directory

--- types/index.ts
+++
@@ -1,6 +0,0 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
-
-export interface IApi {
-    get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
-}
